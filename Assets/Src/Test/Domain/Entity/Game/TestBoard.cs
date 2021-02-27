2fd952b441bc7bf7eacad96f8205b189ff8ef6b2
sinnchan <sinn67c3z42@icloud.com>
2021-02-27 15:08:09 +0900

    指摘対応

--- Assets/Src/Test/Domain/Entity/Game/TestBoard.cs
+++ Assets/Src/Test/Domain/Entity/Game/TestBoard.cs
@@ -1,20 +1,18 @@
+using System;
+using NUnit.Framework;
+using Src.Main.Domain.Entities.Game;
+
 namespace Src.Test.Domain.Entity.Game
 {
-    using System;
-    using System.Linq;
-    using Main.Domain.Entities.Game;
-    using NUnit.Framework;
-    using UnityEditor.VersionControl;
-
     [TestFixture]
     public class TestBoard
     {
         [Test]
         public void IsEmptyTest()
         {
-            Board board = new Board();
+            var board = new Board();
             Assert.AreEqual(board.IsEmpty(), true);
-            board.PlacePiece(new Piece(PieceColor.Black), new Position(1,1));
+            board.PlacePiece(new Piece(PieceColor.Black), new Position(1, 1));
             Assert.AreEqual(board.IsEmpty(), false);
             board.Clear();
             Assert.AreEqual(board.IsEmpty(), true);
@@ -26,34 +24,33 @@ namespace Src.Test.Domain.Entity.Game
         [TestCase(1, 0)]
         [TestCase(9, 9)]
         public void PlacePieceErrorTest(int x, int y)
-        {       
+        {
             var board = new Board();
 
-            Assert.Catch<System.ArgumentOutOfRangeException>(() =>
-                board.PlacePiece(new Piece(PieceColor.Black), new Position(x, y)));
+            Assert.Catch<ArgumentOutOfRangeException>(() =>
+            {
+                board.PlacePiece(new Piece(PieceColor.Black), new Position(x, y));
+            });
         }
-        
+
         [TestCase(1, 1)]
         [TestCase(8, 1)]
         [TestCase(1, 8)]
         [TestCase(8, 8)]
-        [TestCase(2,2)]
+        [TestCase(2, 2)]
         public void PlacePieceTest(int x, int y)
-        {       
+        {
             var board = new Board();
             board.PlacePiece(new Piece(PieceColor.Black), new Position(x, y));
         }
-        
+
         [Test]
         public void IsFullTest()
         {
-            Board board = new Board();
+            var board = new Board();
             Assert.AreEqual(board.IsFull(), false);
             Board.LoopAccessAll((x, y) => { board.Data[y, x] = Piece.CreateBlack(); });
             Assert.AreEqual(board.IsFull(), true);
         }
-        
-
-
     }
 }
